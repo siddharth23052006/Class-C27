72d09bf324f7d841017d937d775106fec253e610
siddharth23052006 <siddharthshivakumar2@gmail.com>
2020-05-31 12:52:49 +0530

    Added a constraint and displayed a chain there.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog, chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -26,7 +28,7 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig2 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -34,7 +36,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    constrainedLog = new Log(200,200,80,PI/2);
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body, constrainedLog.body); 
 
 }
 
@@ -52,7 +57,7 @@ function draw(){
 
     box3.display();
     box4.display();
-    pig3.display();
+    pig2.display();
     log3.display();
 
     box5.display();
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
 }
